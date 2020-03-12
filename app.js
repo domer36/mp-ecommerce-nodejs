7164838ddf0a7b1cbbecec5f2f99f2d4aa77642a
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-03-11 19:06:14 -0600

    done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 var express = require('express');
 var exphbs  = require('express-handlebars');
+const mercadopago = require('mercadopago')
  
 var app = express();
  
@@ -11,7 +13,34 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    const { img, title, price, unit } = req.query
+    
+    mercadopago.configure({
+        access_token: process.env.TOKEN
+    })
+
+    let preference = {
+        "items": [
+            {
+                "title": title,
+                "picture_url": img,
+                "quantity": Number(unit),
+                "unit_price": Number(price)
+            }
+        ]
+    }
+
+    mercadopago.preferences.create( preference )
+    .then( response => {
+        global.init_point = response.body.init_point
+        res.render('detail', {
+            img: req.query.img, title, price, unit, id_preference: response.body.id
+        });
+    })
+    .catch( err => console.log( err ))
+
+
+
 });
 
 app.use(express.static('assets'));
